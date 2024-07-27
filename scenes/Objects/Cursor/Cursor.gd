d4fdb7ba8964ae72e01ff408c4dbba4ae7c84493
Sophie Madi <sophieam701@gmail.com>
2024-07-27 16:03:31 -0500

    Added Cursor
    Hide default mouse and added custom cursor object to all scenes. Saved as class for reusability.

--- scenes/Objects/Cursor/Cursor.gd
+++ scenes/Objects/Cursor/Cursor.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	# Hide mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	# Follow invisible cursor
+	position.x = get_viewport().get_mouse_position().x
+	position.y = get_viewport().get_mouse_position().y
+	
 	pass
