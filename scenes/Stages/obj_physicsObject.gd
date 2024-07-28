05f76b7f4d11f514db1325110bc5b5b2d368d422
Sophie Madi <sophieam701@gmail.com>
2024-07-27 21:13:01 -0500

    Drag-and-drop, very buggy

--- scenes/Stages/obj_physicsObject.gd
+++ scenes/Stages/obj_physicsObject.gd
@@ -2,16 +2,37 @@ extends RigidBody2D
 
 # Local variables
 var canGrab = false
+var grabbed = false
+
 @onready var collideSound = $AudioStreamPlayer2D
+@onready var colorRect = $Placeholder
+@onready var collision = $Collision
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Open channel for collision reporting -- only need one for sonuds
 	max_contacts_reported = 1
+	input_pickable = true
 
 # Called every frame
 func _process(delta):
-	pass
+	
+	# If moused over, can be grabbed
+	if canGrab && Input.is_action_just_pressed("Left-Click"):
+		grabbed = true
+	
+	# If mouse released, end grab regardless of circumstance
+	if Input.is_action_just_released("Left-Click"):
+		grabbed = false
+		
+	# If currently grabbed, follow cursor
+	if grabbed:
+		position.x = get_global_mouse_position().x
+		position.y = get_global_mouse_position().y
+		#collision.disabled = true
+	#else:
+		#collision.disabled = false
 
 # Play sound on collision
 func _on_body_entered(body):
